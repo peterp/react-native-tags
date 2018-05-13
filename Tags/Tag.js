ba3f7c80d2c3f6375dcac4326d4b498af33d67e9
Peter Pistorius <peter.pistorius@gmail.com>
2018-05-13 17:01:24 +0200

    v1.2.1

---
+++ Tags/Tag.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+
+const Tag = ({ label, onPress, tagContainerStyle, tagTextStyle }) => (
+  <TouchableOpacity style={[styles.tag, tagContainerStyle]} onPress={onPress}>
+    <Text style={[styles.tagLabel, tagTextStyle]}>{label}</Text>
+  </TouchableOpacity>
+);
+
+Tag.propTypes = {
+  label: PropTypes.string.isRequired,
+  onPress: PropTypes.func
+};
+
+export default Tag;
