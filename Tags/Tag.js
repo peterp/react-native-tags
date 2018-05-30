49dc5804db38803e47077dac6655a0f7183b8daf
David <djaffe89@gmail.com>
2018-05-29 20:16:03 -0500

    Prevent visual feedback when readonly is true

--- Tags/Tag.js
+++ Tags/Tag.js
@@ -4,8 +4,8 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 
-const Tag = ({ label, onPress, tagContainerStyle, tagTextStyle }) => (
-  <TouchableOpacity style={[styles.tag, tagContainerStyle]} onPress={onPress}>
+const Tag = ({ label, onPress, tagContainerStyle, tagTextStyle, readonly }) => (
+  <TouchableOpacity style={[styles.tag, tagContainerStyle]} onPress={onPress} activeOpacity={readonly ? 1 : 0.2 }>
     <Text style={[styles.tagLabel, tagTextStyle]}>{label}</Text>
   </TouchableOpacity>
 );
