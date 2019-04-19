9dfe392505b78b7f978a59b5e3a32bdf122e8f28
Peter Pistorius <peter.pistorius@gmail.com>
2019-04-19 12:28:36 +0200

    Add ability to use render props.

--- Tags/Tag.js
+++ Tags/Tag.js
@@ -1,31 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 
-const Tag = ({ label, onPress, tagContainerStyle, tagTextStyle, readonly }) => {
-  const tagText = <Text style={[styles.tagLabel, tagTextStyle]}>{label}</Text>;
-
-  if (readonly) {
-    return (
-      <View style={[styles.tag, tagContainerStyle]}>
-        {tagText}
-      </View>
-    )
-  } else {
-    return (
-      <TouchableOpacity style={[styles.tag, tagContainerStyle]} onPress={onPress}>
-        {tagText}
-      </TouchableOpacity>
-    )
-  }
-}
+const Tag = ({ label, onPress, tagContainerStyle, tagTextStyle }) => {
+  return (
+    <TouchableOpacity style={[styles.tag, tagContainerStyle]} onPress={onPress}>
+      <Text style={[styles.tagLabel, tagTextStyle]}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
 
 Tag.propTypes = {
   label: PropTypes.string.isRequired,
-  onPress: PropTypes.func,
-  readonly: PropTypes.bool
+  onPress: PropTypes.func.isRequired
 };
 
 export default Tag;
